0bc3f13a7dcd2889b6a5645ed62f1b98f2a97df4
rom@in <romainlam@hotmail.com>
2019-05-11 18:14:15 -0400

    Ajout d'un produit au panier

--- cart.php
+++ cart.php
@@ -20,4 +20,22 @@ class cart{
   }
   public function getList(){
     return !empty($_SESSION['panier']) ? $_SESSION['panier'] : NULL;
-
+  public function addProduct($id_produit,$libelle_produit,$qte=1,$prix_unitaire_produit=0){
+    if($qte > 0 ){
+      $_SESSION['panier'][$id_produit] = array('id_produit'=>$id_produit
+                                                ,'produit'=>$libelle_produit
+                                                ,'qte'=>$qte
+                                                ,'prix_unitaire'=>$prix_unitaire_produit
+                                                ); 
+      $this->updateTotalPriceProduct($id_produit);
+    }else{
+      return "ERREUR : Vous ne pouvez pas ajouter un produit sans quantité..."; 
+    }
+  }
+  
+  private function updateTotalPriceProduct($id_produit){
+    if(isset($_SESSION['panier'][$id_produit])){
+      $_SESSION['panier'][$id_produit]['prix_Total'] = $_SESSION['panier'][$id_produit]['qte'] * $_SESSION['panier'][$id_produit]['prix_unitaire'];
+    }
+  }
+  
